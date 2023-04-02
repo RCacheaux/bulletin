1333553b7c593338c811650b778d49a51300c566
Rene Cacheaux <rcacheaux@Renes-Air.attlocal.net>
2023-04-01 19:35:22 -0500

    Initial Xcode project.

---
+++ Bulletin/Bulletin/BulletinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletinApp.swift
+//  Bulletin
+//
+//  Created by Rene Cacheaux on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BulletinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
